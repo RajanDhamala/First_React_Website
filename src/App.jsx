0a24a6034e5c93c9c9a4f883fdc5437fc5b3b7a9
rajan <rajandhamala0123@gmail.com>
2024-08-10 19:16:13 +0545

    pushing my first react website with the practical use of react router

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './Componnents/Navbar'
+import Home from './Componnents/Home.jsx'
+
+function App() {
+
+
+  return (
+    <>
+   <Navbar/>
+   <Home/>
+    </>
+  )
+}
+
+export default App
