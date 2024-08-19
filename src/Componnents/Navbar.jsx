07c54b30af8606b47778f4739c41393a56f39c94
rajan <rajandhamala0123@gmail.com>
2024-08-19 21:27:27 +0545

    updating with game histry feature

--- src/Componnents/Navbar.jsx
+++ src/Componnents/Navbar.jsx
@@ -59,6 +59,9 @@ export default function Navbar(){
             <li>
               <Link to="/tournaments" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Tournaments</Link>
             </li>
+            <li>
+              <Link to="/games" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Search Games</Link>
+            </li>
           </ul>
         </div>
         </div>
